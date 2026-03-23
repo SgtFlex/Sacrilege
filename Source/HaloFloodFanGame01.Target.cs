f8cfdab0028ed179694752be6f7a6bc6a58bc036
Aaron Ramos <sgtflexxx@gmail.com>
2026-03-23 15:20:04 -0700

    Updated to 5.7, Enabled StateTrees

--- Source/HaloFloodFanGame01.Target.cs
+++ Source/HaloFloodFanGame01.Target.cs
@@ -8,8 +8,8 @@ public class HaloFloodFanGame01Target : TargetRules
 	public HaloFloodFanGame01Target(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("HaloFloodFanGame01");
 	}
 }
--- Source/HaloFloodFanGame01Editor.Target.cs
+++ Source/HaloFloodFanGame01Editor.Target.cs
@@ -8,8 +8,8 @@ public class HaloFloodFanGame01EditorTarget : TargetRules
 	public HaloFloodFanGame01EditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("HaloFloodFanGame01");
 	}
 }
